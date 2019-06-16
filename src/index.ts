58fba7dcbee3ead2be431160037a13bc619930f6
limengke <752140235@qq.com>
2019-06-16 22:05:03 +0800

    refactor: 重构核心代码

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,5 @@
-import { getSpaces } from './util'
-export interface option {
-    space?: number
-    withInterface?: boolean
-}
-export const sg2ts = function (str: string, option:option = {space: 2, withInterface: true}): string {
-    const {
-        space,
-        withInterface
-    } = option
-    const reg: RegExp = /^\s*(\w+)\s*\(([\[\]\w]+),?\s*(\w+)?\):?(.+)?/g
-    const lines = str.split('\n')
-    const result = lines.map(line => {
-        return line.replace(reg, (_, name, type, optional, comment) => {
-            const optionalString = optional ? '?:' : ':'
-            const typeString = getTypeString(type)
-            const commentString = comment ? ` //${comment}` : ''
-            return getSpaces(space) + name + optionalString + ' ' + typeString + commentString
-        })
-    })
-    return result.join('\n')
-}
+import { Resolver, Ioption } from './resolver'
 
-const getTypeString = (type: string): string => {
-    if (isArray(type)){
-        return parseArray(type)
-    }
-    switch (type) {
-    case 'integer':
-        return 'number'
-    default:
-        return type
-    }
-}
-
-const arrReg: RegExp = /array\[(\w+)]$/g
-const isArray = (str: string): boolean => {
-    return arrReg.test(str)
-}
-
-const parseArray = (str: string): string => {
-    return str.replace(arrReg, (_, generics) => {
-        return `Array<${generics}>`
-    })
+export const sg2ts = function (str: string, option?: Ioption): string {
+    return Resolver.getInstance(str, option).getResult()
 }
---
+++ src/resolver.ts
@@ -0,0 +1,83 @@
+import {getSpaces} from './util'
+
+export interface Ioption {
+    space?: number
+    withInterface?: boolean
+}
+const defaultOption: Partial<Ioption> = {
+    space: 4,
+    withInterface: true
+}
+
+export class Resolver {
+    private option: Ioption
+    private source: string = ''
+    private sourceLines: string[] = []
+    private resultLines: string[] = []
+
+    private bodyReg: RegExp = /^\s*(\w+)\s*\(([\[\]\w]+),?\s*(\w+)?\):?(.+)?/g
+    private arrReg: RegExp = /array\[(\w+)]$/g
+
+
+    constructor(source: string, option?: Ioption) {
+        this.source = source
+        if (option) {
+            this.option = Object.assign(defaultOption, option)
+        } else {
+            this.option = defaultOption
+        }
+    }
+
+    getResult (): string {
+        if (!this.result) {
+            this.run()
+        }
+        return this.result
+    }
+
+    get result (): string {
+        return this.resultLines.join('\n')
+    }
+
+    run () {
+        const { space } = this.option
+        this.sourceLines = this.source.split('\n')
+        this.resultLines = this.sourceLines.map(line => {
+            return line.replace(this.bodyReg, (_, name, type, optional, comment) => {
+                const optionalString = optional ? '?:' : ':'
+                const typeString = this.getTypeString(type)
+                const commentString = comment ? ` //${comment}` : ''
+                return getSpaces(space) + name + optionalString + ' ' + typeString + commentString
+            })
+        })
+    }
+
+    static getInstance (source: string, option?: Ioption) {
+        return new Resolver(source, option)
+    }
+
+    getTypeString (type: string): string {
+        if (this.isArray(type)){
+            return this.parseArray(type)
+        }
+        switch (type) {
+        case 'integer':
+            return 'number'
+        default:
+            return type
+        }
+    }
+
+    parseArray (str: string): string {
+        return str.replace(this.arrReg, (_, generics) => {
+            return `Array<${generics}>`
+        })
+    }
+
+    isArray (str: string): boolean {
+        return this.arrReg.test(str)
+    }
+
+}
+
+
